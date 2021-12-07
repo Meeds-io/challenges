56345b6dfcfe69d9c2bf04e9ee71a257fd2acb55
GouadriaHanen <49310007+GouadriaHanen@users.noreply.github.com>
2021-12-07 14:41:40 +0100

    TASK-51210 :  First screen of the App (#5)

--- challenges-webapp/src/main/webapp/vue-app/challenges/components/Challenges.vue
+++ challenges-webapp/src/main/webapp/vue-app/challenges/components/Challenges.vue
@@ -1,8 +1,37 @@
 <template>
   <v-app
-    class="challenges-application border-box-sizing transparent"
+    class="challenges-application border-box-sizing"
     role="main"
     flat>
-    <span>Challenges Application</span>
+    <v-toolbar
+      color="transparent"
+      flat
+      class="pa-4">
+      <add-challenge v-if="canAddChallenge" />
+      <v-spacer />
+    </v-toolbar>
+    <template>
+      <welcome-message
+        v-if="!displayChallenges"
+        :can-add-challenge="canAddChallenge" />
+    </template>
   </v-app>
 </template>
+<script>
+export default {
+  data: () => ({
+    canAddChallenge: false,
+    challenges: [],
+  }),
+  computed: {
+    displayChallenges() {
+      return this.challenges && this.challenges.length > 0 ;
+    }
+  },
+  created() {
+    this.$challengesServices.canAddChallenge().then(canAddChallenge => {
+      this.canAddChallenge = canAddChallenge;
+    });
+  }
+};
+</script>
--- challenges-webapp/src/main/webapp/vue-app/challenges/initComponents.js
+++ challenges-webapp/src/main/webapp/vue-app/challenges/initComponents.js
@@ -1,9 +1,19 @@
 import Challenges from './components/Challenges.vue';
-
+import AddChallenge from './components/AddChallenge.vue';
+import WelcomeMessage from './components/WelcomeMessage.vue';
 const components = {
   'challenges': Challenges,
+  'add-challenge': AddChallenge,
+  'welcome-message': WelcomeMessage,
 };
 
 for (const key in components) {
   Vue.component(key, components[key]);
+}
+import * as  challengesServices from './challengesServices';
+
+if (!Vue.prototype.$challengesServices) {
+  window.Object.defineProperty(Vue.prototype, '$challengesServices', {
+    value: challengesServices,
+  });
 }
