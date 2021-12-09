7543ce75ebb2a146ad288279327f842c93df46ea
Mohamed El Hadi Rdid <54801286+Rdidmohamedelhadi@users.noreply.github.com>
2021-12-09 14:51:13 +0100

    Task-50950: US02 Challenge CARD (#8)

--- challenges-webapp/src/main/webapp/vue-app/challenges/components/Challenges.vue
+++ challenges-webapp/src/main/webapp/vue-app/challenges/components/Challenges.vue
@@ -1,6 +1,7 @@
 <template>
   <v-app
     class="challenges-application border-box-sizing"
+    :class="classWelcomeMessage"
     role="main"
     flat>
     <v-toolbar
@@ -10,9 +11,13 @@
       <add-challenge v-if="canAddChallenge" />
       <v-spacer />
     </v-toolbar>
-    <template>
+    <template v-if="displayChallenges">
+      <div class="pl-2 pt-5">
+        <challenges-list :challenges="challenges" />
+      </div>
+    </template>
+    <template v-else>
       <welcome-message
-        v-if="!displayChallenges"
         :can-add-challenge="canAddChallenge" />
     </template>
   </v-app>
@@ -22,15 +27,20 @@ export default {
   data: () => ({
     canAddChallenge: false,
     challenges: [],
+    displayChallenges: true
   }),
   computed: {
-    displayChallenges() {
-      return this.challenges && this.challenges.length > 0 ;
+    classWelcomeMessage() {
+      return !this.displayChallenges ? 'emptyChallenges': '';
     }
   },
   created() {
     this.$challengesServices.canAddChallenge().then(canAddChallenge => {
       this.canAddChallenge = canAddChallenge;
+    });
+    this.$challengesServices.getAllChallengesByUser().then(challenges => {
+      this.challenges = challenges;
+      this.displayChallenges = this.challenges && this.challenges.length > 0 ? true : false;
     });
   }
 };
--- challenges-webapp/src/main/webapp/vue-app/challenges/initComponents.js
+++ challenges-webapp/src/main/webapp/vue-app/challenges/initComponents.js
@@ -1,10 +1,14 @@
 import Challenges from './components/Challenges.vue';
 import AddChallenge from './components/AddChallenge.vue';
 import WelcomeMessage from './components/WelcomeMessage.vue';
+import ChallengeCard from './components/ChallengeCard.vue';
+import ChallengesList from './components/ChallengesList.vue';
 const components = {
   'challenges': Challenges,
   'add-challenge': AddChallenge,
   'welcome-message': WelcomeMessage,
+  'challenge-card': ChallengeCard,
+  'challenges-list': ChallengesList
 };
 
 for (const key in components) {
