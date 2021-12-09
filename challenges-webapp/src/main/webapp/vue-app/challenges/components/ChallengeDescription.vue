cd2ba72f24984edfe83f12f196bccc0399a1bbeb
GouadriaHanen <49310007+GouadriaHanen@users.noreply.github.com>
2021-12-09 16:25:06 +0100

    TASK-50951 : add challenge (#7)
    (cherry picked from commit 1797e0fcc8921831b731a38596c10f733fcb05b5)

---
+++ challenges-webapp/src/main/webapp/vue-app/challenges/components/ChallengeDescription.vue
@@ -0,0 +1,115 @@
+<template>
+  <div
+    id="challengeDescriptionId"
+    class="challengeDescription">
+    <div class="py-1 px-2 subtitle-1">
+      {{ $t('challenges.label.describeYourChallenge') }}
+    </div>
+    <textarea
+      id="descriptionContent"
+      ref="editor"
+      v-model="inputVal"
+      cols="30"
+      rows="10"></textarea>
+    <span class="tooManyChars">
+      {{ charsCount }}{{ maxLength > -1 ? ' / ' + maxLength : '' }}
+      <i class="uiIconMessageLength"></i>
+    </span>
+  </div>
+</template>
+
+<script>
+import {urlVerify} from '../challengesUtils.js';
+export default {
+  props: {
+    value: {
+      type: String,
+      default: ''
+    },
+    challenge: {
+      type: Object,
+      default: function () {
+        return {};
+      }
+    }
+  },
+  data() {
+    return {
+      inputVal: this.value,
+      charsCount: 0,
+      editorReady: false,
+      maxLength: 1300,
+    };
+  },
+  computed: {
+    challengeDescription () {
+      return this.challenge && this.challenge.id && this.challenge.description || '';
+    }
+  },
+  watch: {
+    inputVal(val) {
+      return this.urlVerify(val);
+    },
+    value() {
+      this.inputVal = this.challengeDescription;
+    },
+    reset() {
+      CKEDITOR.instances['descriptionContent'].destroy(true);
+      this.editorReady = false;
+    },
+    charsCount() {
+      if (this.charsCount > this.maxLength) {
+        this.$emit('disableCreateButton');
+      }
+    },
+  },
+  created() {
+    this.initCKEditor();
+  },
+  methods: {
+    initCKEditor: function () {
+      CKEDITOR.plugins.addExternal('embedsemantic', '/commons-extension/eXoPlugins/embedsemantic/', 'plugin.js');
+      let extraPlugins = 'simpleLink,widget,embedsemantic';
+      const windowWidth = $(window).width();
+      const windowHeight = $(window).height();
+      if (windowWidth > windowHeight && windowWidth < 768) {
+        extraPlugins = 'simpleLink,selectImage,embedsemantic';
+      }
+      CKEDITOR.basePath = '/commons-extension/ckeditor/';
+      const self = this;
+      $(this.$refs.editor).ckeditor({
+        customConfig: '/commons-extension/ckeditorCustom/config.js',
+        //removePlugins: 'suggester,simpleLink,confighelper',
+        extraPlugins: extraPlugins,
+        removePlugins: 'confirmBeforeReload,maximize,resize',
+        toolbarLocation: 'bottom',
+        autoGrow_onStartup: true,
+        on: {
+          blur: function () {
+            $(document.body).trigger('click');
+          },
+          change: function(evt) {
+            const newData = evt.editor.getData();
+            self.inputVal = newData;
+            let pureText = newData ? newData.replace(/<[^>]*>/g, '').replace(/&nbsp;/g, '').trim() : '';
+            const div = document.createElement('div');
+            div.innerHTML = pureText;
+            pureText = div.textContent || div.innerText || '';
+            self.charsCount = pureText.length;
+          },
+          destroy: function () {
+            self.inputVal = '';
+            self.charsCount = 0;
+          }
+        },
+      });
+    },
+
+    urlVerify(text) {
+      text = urlVerify(text);
+      this.$emit('addChallengeDescription',text);
+      return text;
+    }
+  }
+};
+</script>
