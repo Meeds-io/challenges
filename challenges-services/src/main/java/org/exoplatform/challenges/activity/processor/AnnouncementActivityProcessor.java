8ea5b8f27ae58ecc0c7cfbae1fdd355f8a0180a2
GouadriaHanen <hgouadria@exoplatform.com>
2021-12-23 11:35:46 +0100

    TASK-52426: add challenge announcement List to challengeRestEntity

---
+++ challenges-services/src/main/java/org/exoplatform/challenges/activity/processor/AnnouncementActivityProcessor.java
@@ -0,0 +1,71 @@
+package org.exoplatform.challenges.activity.processor;
+
+import org.exoplatform.challenges.model.Announcement;
+import org.exoplatform.challenges.model.UserInfo;
+import org.exoplatform.challenges.service.AnnouncementService;
+import org.exoplatform.challenges.utils.Utils;
+import org.exoplatform.commons.exception.ObjectNotFoundException;
+import org.exoplatform.container.xml.InitParams;
+import org.exoplatform.services.log.ExoLogger;
+import org.exoplatform.services.log.Log;
+import org.exoplatform.social.core.BaseActivityProcessorPlugin;
+import org.exoplatform.social.core.activity.model.ExoSocialActivity;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class AnnouncementActivityProcessor extends BaseActivityProcessorPlugin {
+
+  private static final Log LOG = ExoLogger.getLogger(AnnouncementActivityProcessor.class);
+
+  AnnouncementService      announcementService;
+
+  public AnnouncementActivityProcessor(InitParams params, AnnouncementService announcementService) {
+    super(params);
+    this.announcementService = announcementService;
+  }
+
+  @Override
+  public void processActivity(ExoSocialActivity activity) {
+    if (activity.isComment() || activity.getType() == null) {
+      return;
+    }
+    String announcementId = activity.getTemplateParams().get("announcementId");
+    try {
+      Announcement announcement = announcementService.getAnnouncementById(Long.parseLong(announcementId));
+
+      Map<String, String> params = new HashMap<>();
+      params.put("announcementAssigneeUsername", this.getAssigneeUserNames(announcement.getAssignee()));
+      params.put("announcementAssigneeFullName", this.getAssigneeFullNames(announcement.getAssignee()));
+      activity.getTemplateParams().putAll(params);
+    } catch (ObjectNotFoundException e) {
+      LOG.error("Unexpected error", e);
+    }
+  }
+
+  private String getAssigneeFullNames(List<Long> assignee) {
+    if (assignee.isEmpty()) {
+      throw new IllegalArgumentException("announcement assignee must have at least one winner");
+    }
+    String AssigneeFullNames = "";
+    List<UserInfo> AssigneeIdentityList = Utils.getUsersByIds(assignee);
+    for (UserInfo user : AssigneeIdentityList) {
+      AssigneeFullNames = AssigneeFullNames + user.getFullName() + "#";
+    }
+    return AssigneeFullNames;
+  }
+
+  private String getAssigneeUserNames(List<Long> assignee) {
+    if (assignee.isEmpty()) {
+      throw new IllegalArgumentException("announcement assignee must have at least one winner");
+    }
+    String AssigneeUserNames = "";
+    List<UserInfo> AssigneeIdentityList = Utils.getUsersByIds(assignee);
+    for (UserInfo user : AssigneeIdentityList) {
+      AssigneeUserNames = AssigneeUserNames + user.getRemoteId() + "#";
+    }
+    return AssigneeUserNames;
+  }
+
+}
